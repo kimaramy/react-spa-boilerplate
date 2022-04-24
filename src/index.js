72a6f483576230d94502572a9a3d6f0552c78a88
Haram Kim <fed.haram@gmail.com>
2022-04-25 03:01:15 +0900

    chore: css 파일 이동 (src/assets/css)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './app/App'
 import 'normalize.css'
 import 'nprogress/nprogress.css'
-import './assets/custom-nprogress.css'
-import './index.css'
+import './assets/css/custom-nprogress.css'
+import './assets/css/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
