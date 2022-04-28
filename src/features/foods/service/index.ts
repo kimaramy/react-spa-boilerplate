9b915070995b020b8957eb44327f0358be51ef3b
Haram Kim <fed.haram@gmail.com>
2022-04-28 23:02:45 +0900

    feature: mock 서버 구성 (#2)

--- src/features/foods/service/index.ts
+++ src/features/foods/service/index.ts
@@ -2,18 +2,18 @@ import axios from 'axios'
 import type { Food, FoodDetail } from './food'
 
 const foodAPI = axios.create({
-  baseURL: 'https://nrisecodingtest.s3.ap-northeast-2.amazonaws.com/fe/food',
+  baseURL: 'http://localhost:8080/',
   timeout: 5000,
 })
 
 const foodService = {
   getFoods: async () => {
     // 에러 캐치와 함께 에러 바운더리로 버블링
-    const { data } = await foodAPI.get<Food[]>('/food_main_list.json')
+    const { data } = await foodAPI.get<Food[]>('/foods')
     return data
   },
   getFoodDetails: async () => {
-    const { data } = await foodAPI.get<FoodDetail[]>('/food_detail_list.json')
+    const { data } = await foodAPI.get<FoodDetail[]>('/foodDetails')
     return data
   },
 }
