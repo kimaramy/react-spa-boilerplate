f1c961400e979cae06b86307e9e8258c32ec0318
Haram Kim <fed.haram@gmail.com>
2022-04-30 21:43:02 +0900

    chore: 공통 axios 모듈 호출 방식으로 디렉토리 변경

--- src/features/foods/service/index.ts
+++ src/features/foods/service/index.ts
@@ -1,29 +1,5 @@
-import axios from 'axios'
-import nprogress from 'nprogress'
+import axios from '@/service/axios'
 import type { Food, FoodDetail } from './food'
-
-nprogress.configure({ showSpinner: false, speed: 500 })
-
-axios.defaults.onDownloadProgress = (e: ProgressEvent) => {
-  const percentage = Math.floor(e.loaded * 1.0) / e.total
-  nprogress.set(percentage)
-}
-
-axios.defaults.onUploadProgress = (e: ProgressEvent) => {
-  const percentage = Math.floor(e.loaded * 1.0) / e.total
-  nprogress.set(percentage)
-}
-
-axios.interceptors.response.use(
-  (response) => {
-    nprogress.done()
-    return response
-  },
-  (error) => {
-    nprogress.done()
-    return Promise.reject(error)
-  },
-)
 
 const foodAPI = axios.create({
   baseURL: 'http://localhost:8080/',
